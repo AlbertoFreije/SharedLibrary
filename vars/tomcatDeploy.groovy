3e1759dd644fdbe94713980d5ea7bce0d0a30fe9
AlbertoFreije <freicartobe@gmail.com>
2022-04-19 11:35:46 +0200

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
 
-def call() {
-   deploy adapters: [tomcat9(credentialsId: 'war-deployer', path: '', url: 'http://192.168.56.10:8080')], contextPath: 'calculadora', war: '**/*.war'
+def call(String credentialsId = 'war-deployer') {
+   deploy adapters: [tomcat9(credentialsId: ${credentialsId}, path: '', url: 'http://192.168.56.10:8080')], contextPath: 'calculadora', war: '**/*.war'
 }

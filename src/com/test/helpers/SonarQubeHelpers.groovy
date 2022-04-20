0b1af1a37dead67ddfcd6c7d46c390932428da7f
AlbertoFreije <freicartobe@gmail.com>
2022-04-20 12:31:59 +0200

    public

--- src/com/test/helpers/SonarQubeHelpers.groovy
+++ src/com/test/helpers/SonarQubeHelpers.groovy
@@ -1,7 +1,6 @@
-#!/usr/bin/env groovy
 package com.test.helpers
 
-class SonarQubeHelpers {
+public class SonarQubeHelpers {
    public static final String foo = 'sonar-scanner'
    public final static int time = 10
    public final static int unit = 'MINUTES'

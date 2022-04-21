36882665ae32b123bbfdf91c22a8dd7b0f1448b0
AlbertoFreije <freicartobe@gmail.com>
2022-04-21 12:07:01 +0200

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -5,9 +5,11 @@ def call(String credentials = 'war-deployer', String context = 'calculadora') {
    def config = [:]
    config.groupId = readMavenPom().getGroupId()
    config.artifactId = readMavenPom().getArtifactId()
+   config.version = readMavenPom().getVersion()
 
    println("El GroupId es " + config.groupId)
    println("El artifactId es " + config.artifactId)
+   println("Version " + config.version)
 
    deploy adapters: [tomcat9(credentialsId: "${credentials}", path: '', url: 'http://192.168.56.10:8080')], contextPath: "${context}", war: '**/*.war'
 }

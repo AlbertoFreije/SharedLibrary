4fce522e93ea1ab053bd20f942dbd62a44bde053
AlbertoFreije <freicartobe@gmail.com>
2022-04-21 09:44:53 +0200

    Update mavenBuild.groovy

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -2,7 +2,12 @@
 
 def call() {
 
+ def mvnHome = tool 'Maven 3.3.9'
+ def mvnExec = mvnHome + "/bin/mvn"
+
  sh "mvn clean package"
- sh "mvn clean deploy -U -Dmaven.test.skip=true"
+ //sh "mvn clean deploy -U -Dmaven.test.skip=true"
+
+ sh ("${mvnExec} -B deploy:deploy-file")
 
 }

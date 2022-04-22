b8a94f30ebd58ab9baec3480df4c5a1bba5afd03
AlbertoFreije <freicartobe@gmail.com>
2022-04-22 10:03:40 +0200

    lll

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -11,7 +11,6 @@ def call(String credentials = 'war-deployer', String context = 'calculadora') {
    println("El artifactId es " + config.artifactId)
    println("Version " + config.version)
 
-   sh "mvn dependency:get -DremoteRepositories=http://192.168.56.10:8881/repository/maven-releases/org/springframework/calculadora/0.1.0/calculadora-0.1.0.war"
 
    deploy adapters: [tomcat9(credentialsId: "${credentials}", path: '', url: 'http://192.168.56.10:8080')], contextPath: "${config.artifactId}", war: '**/*.war'
 }

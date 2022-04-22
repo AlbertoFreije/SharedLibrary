fb2e70d6a3da2343433ed46a8bb594dd6602e5e9
AlbertoFreije <freicartobe@gmail.com>
2022-04-22 10:10:45 +0200

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -11,7 +11,7 @@ def call(String credentials = 'war-deployer', String context = 'calculadora') {
    println("El artifactId es " + config.artifactId)
    println("Version " + config.version)
 
-   //sh "mvn dependency:get -DremoteRepositories=http://192.168.56.10:8881/repository/maven-releases/org/springframework/calculadora/0.1.0/calculadora-0.1.0.war -DgroupId=org.springframework -DartifactId=calculadora -Dversion=0.1.0 -DType=war"
+   sh "mvn dependency:get -DremoteRepositories=http://192.168.56.10:8881/repository/maven-releases/org/springframework/calculadora/0.1.0/calculadora-0.1.0.war -DgroupId=org.springframework -DartifactId=calculadora -Dversion=0.1.0 -DType=war"
    
-   deploy adapters: [tomcat9(credentialsId: "${credentials}", path: '', url: 'http://192.168.56.10:8080')], contextPath: "${config.artifactId}", war: 'http://192.168.56.10:8881/repository/maven-releases/org/springframework/calculadora/0.1.0/calculadora-0.1.0.war'
+   deploy adapters: [tomcat9(credentialsId: "${credentials}", path: '', url: 'http://192.168.56.10:8080')], contextPath: "${config.artifactId}", war: 'http://192.168.56.10:8881/repository/maven-releases/org/springframework/calculadora/0.1.0/*.war'
 }

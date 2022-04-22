8e108af1b94c88c792e1d548c30d449d6d129851
AlbertoFreije <freicartobe@gmail.com>
2022-04-22 10:06:51 +0200

    cambios

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -6,7 +6,6 @@ def call() {
  def mvnExec = mvnHome + "/bin/mvn"
 
  sh "mvn clean package"
- sh "mvn dependency:get -DremoteRepositories=http://192.168.56.10:8881/repository/maven-releases/ -DgroupId=org.springframework -DartifactId=calculadora -Dversion=0.1.0 -DType=war"
  sh "mvn clean deploy -U -Dmaven.test.skip=true"
 
  //sh ("${mvnExec} -B deploy:deploy-file")

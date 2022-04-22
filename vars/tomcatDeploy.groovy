d0ff478ed56b0a8a6318c612ce72c38ddfb96720
AlbertoFreije <freicartobe@gmail.com>
2022-04-22 11:26:11 +0200

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -11,11 +11,11 @@ def call(String credentials = 'war-deployer', String context = 'calculadora') {
    println("El artifactId es " + config.artifactId)
    println("Version " + config.version)
 
-   sh "mvn dependency:get -DremoteRepositories=http://192.168.56.10:8881/repository/maven-releases/ -DgroupId=org.springframework -DartifactId=calculadora -Dversion=0.1.0 -Dpackaging=war -DType=war"
+   //sh "mvn dependency:get -DremoteRepositories=http://192.168.56.10:8881/repository/maven-releases/ -DgroupId=org.springframework -DartifactId=calculadora -Dversion=0.1.0 -Dpackaging=war -DType=war"
    
    //sh "mvn dependency:copy -Dartifact=org.springframework:calculadora:0.1.0 -DoutputDirectory=./target" 
    //sh "mvn dependency:copy -DremoteRepositories=http://192.168.56.10:8881/repository/maven-releases/ -DgroupId=org.springframework -DartifactId=calculadora -Dversion=0.1.0 -Dpackaging=war -DType=war -DoutputDirectory=./target" 
-   sh "mvn dependency:copy-dependencies"
+   //sh "mvn dependency:copy-dependencies"
    
    //deploy adapters: [tomcat9(credentialsId: "${credentials}", path: '', url: 'http://192.168.56.10:8080')], contextPath: "${config.artifactId}", war: './target/calculadora-0.1.0.war'
 }

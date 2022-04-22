188be4364b3889026e75586d4ca67026b9635402
AlbertoFreije <freicartobe@gmail.com>
2022-04-22 11:11:37 +0200

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -13,7 +13,8 @@ def call(String credentials = 'war-deployer', String context = 'calculadora') {
 
    sh "mvn dependency:get -DremoteRepositories=http://192.168.56.10:8881/repository/maven-releases/ -DgroupId=org.springframework -DartifactId=calculadora -Dversion=0.1.0 -Dpackaging=war -DType=war"
    
-   sh "mvn dependency:copy -Dartifact=org.springframework:calculadora:0.1.0 -DoutputDirectory=./target" 
+   //sh "mvn dependency:copy -Dartifact=org.springframework:calculadora:0.1.0 -DoutputDirectory=./target" 
+   sh "mvn dependency:copy -DremoteRepositories=http://192.168.56.10:8881/repository/maven-releases/ -DgroupId=org.springframework -DartifactId=calculadora -Dversion=0.1.0 -Dpackaging=war -DType=war -DoutputDirectory=./target" 
    
    deploy adapters: [tomcat9(credentialsId: "${credentials}", path: '', url: 'http://192.168.56.10:8080')], contextPath: "${config.artifactId}", war: './target/calculadora-0.1.0.war'
 }
